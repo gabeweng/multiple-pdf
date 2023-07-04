dcfac01908f1cfe450ffe998e4765ef64a2fb436
Gabriel Weng <gabeweng@gmail.com>
2023-07-04 08:29:37 -0400

    adding conversation chain

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
-
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -29,9 +31,22 @@ def get_vectorstore(text_chunks):
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
+def get_conversation_chain(vectorstore):
+    llm= ChatOpenAI()
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
 def main():
     load_dotenv()
     st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
 
     st.header("Chat with multiple PDFs :books:")
     st.text_input("Ask a question about your documents:")
@@ -46,6 +61,11 @@ def main():
                 text_chunks = get_text_chunks(raw_text)
 
                 vectorstore = get_vectorstore(text_chunks)
+
+                st.session_state.conversation = get_conversation_chain(vectorstore)
+
+
+
 
 
 if __name__ == '__main__':
